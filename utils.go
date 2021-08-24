a1a3fe9571c08eee37d539ee85cc131d4c947b13
thanhn-inc <thanh.n@incognito.org>
2021-08-24 11:42:34 +0100

    Add prompts

--- utils.go
+++ utils.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"github.com/incognitochain/go-incognito-sdk-v2/incclient"
+	"golang.org/x/crypto/ssh/terminal"
+	"log"
+	"os"
+	"reflect"
+	"strings"
+	"time"
 )
 
 var network string
@@ -63,4 +71,67 @@ func initClient(rpcHost string, version int) error {
 
 	cfg.incClient = incClient
 	return nil
+}
+
+// promptInput asks for input from the user and saves input to `response`.
+// If isSecret is `true`, it will not echo user's input on the terminal.
+func promptInput(message string, response interface{}, isSecret ...bool) ([]byte, error) {
+	fmt.Printf("%v %v: ", time.Now().Format("2006/01/02 15:04:05"), message)
+
+	var input []byte
+	var err error
+	if len(isSecret) > 0 && isSecret[0] {
+		input, err = terminal.ReadPassword(0)
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println()
+	} else {
+		reader := bufio.NewReader(os.Stdin)
+		tmpInput, err := reader.ReadString('\n')
+		if err != nil {
+			return nil, err
+		}
+		tmpInput = parseInput(tmpInput)
+		input = []byte(tmpInput)
+	}
+
+	switch reflect.TypeOf(response).String() {
+	case "*string", "string":
+		response = string(input)
+	default:
+		err = json.Unmarshal(input, response)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return input, nil
+}
+
+// yesNoPrompt asks for a yes/no decision from the user.
+func yesNoPrompt(message string) {
+	fmt.Printf("%v %v (y/n): ", time.Now().Format("2006/01/02 15:04:05"), message)
+
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	input = parseInput(input)
+
+	if !strings.Contains(input, "y") && !strings.Contains(input, "Y") {
+		log.Fatal("Abort!!")
+	}
+}
+
+func parseInput(text string) string {
+	if len(text) == 0 {
+		return text
+	}
+	if text[len(text) - 1] == 13 || text[len(text) - 1] == 10{
+		text = text[:len(text) - 1]
+	}
+
+	return text
 }
